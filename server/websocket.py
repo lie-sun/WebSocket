d64ea26bd9df6b8b61d18cd589a7a6a824aa3697
lie-sun <1466548001@qq.com>
2018-11-14 16:34:27 +0800

    server

--- server/websocket.py
+++ server/websocket.py
@@ -6,9 +6,20 @@ def new_client(client, server):
     server.send_message_to_all("Hey all, a new client has joined us")
 
 
+def message_received(client, server, message):
+    print("ClientNo(%d) said: %s" % (client['id'], message))
+    # print(WebsocketServer.clients)
+    if (len(WebsocketServer.clients) >= 2):
+        server.send_message(WebsocketServer.clients[1], message)
+
+
+def client_left(client, server):
+    print('ClientNo(%d) left' % client['id'])
+
+
 PORT = 9001
-server = WebsocketServer(PORT, "0.0.0.0")
+server = WebsocketServer(PORT, '0.0.0.0')
 server.set_fn_new_client(new_client)
-# server.set_fn_client_left(client_left)
-# server.set_fn_message_received(message_received)
+server.set_fn_message_received(message_received)
+server.set_fn_client_left(client_left)
 server.run_forever()
